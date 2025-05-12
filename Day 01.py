3490f2faee335913b8f428416038c882522a7ac1
IrayChang <feng-jui.chang@outlook.com>
2025-05-12 14:25:03 +0100

    Initial commit: Day 1 - Band Name Generator

---
+++ Day 01.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?")
+pets_name = input("What's your pets name?")
+
+print("Your Band name could be " + city_name + " " + pets_name)
